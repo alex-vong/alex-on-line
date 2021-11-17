14d00e13cd614faef3f0e2adbf5a1119dca0a730
Alex Vong <avong003@gmail.com>
2021-11-16 20:50:32 -0800

    created home

---
+++ home.php
@@ -0,0 +1,19 @@
+<section class="home">
+	<inner-column>
+		<div class="landing container">
+			<h2 class="section-heading loud-voice">Hi, I'm Alex</h2>
+
+			<p class="calm-voice intro">
+				I am a developer passionate about creating amazing online experiences through <span>Design</span> and <span>Development</span>. This is my site where I share all the projects I've worked on and talk about them. 
+			</p>
+
+			<p class="quiet-voice">
+				Currently seeking employment as a <span>Front-End Web Developer</span>
+			</p>
+
+		</div>
+
+		<?php include("modules/contact-me.php") ?>
+
+	</inner-column>
+</section>
